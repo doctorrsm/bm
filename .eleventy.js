17912f5ef5d15c37735d08aaa91d831676dfa079
Sergey Rutkovsky <dr.bibelman@gmail.com>
2022-04-20 07:25:54 +0300

    сохраняет изменения

--- .eleventy.js
+++ .eleventy.js
@@ -1,41 +1,35 @@
 const sitemap = require("@quasibit/eleventy-plugin-sitemap");
+const shuffle  = require("just-shuffle");
+const nunjucks = require('nunjucks');
+
+
+
 module.exports = function(eleventyConfig) {
-    //eleventyConfig.addPassthroughCopy("./src/images");
-    eleventyConfig.addPassthroughCopy("./src/css");
-    eleventyConfig.addPassthroughCopy("./src/js");
-    eleventyConfig.addPassthroughCopy("./src/robots.txt");
-    eleventyConfig.addPassthroughCopy("./src/.htaccess");
+  //eleventyConfig.addPassthroughCopy("./src/images");
+  eleventyConfig.addPassthroughCopy("./src/css");
+  eleventyConfig.addPassthroughCopy("./src/js");
+  eleventyConfig.addPassthroughCopy("./src/robots.txt");
+  eleventyConfig.addPassthroughCopy("./src/.htaccess");
 
 
-    eleventyConfig.addPlugin(sitemap, {
-      sitemap: {
-        hostname: "http://bible-mystery.org",
-      },
-    });
+  eleventyConfig.addPlugin(sitemap, {
+    sitemap: {
+      hostname: "https://bible-mystery.org",
+    },
+  });
+
+  eleventyConfig.addFilter("listSlice", (list, start, end) => list.slice(start, end));
+  eleventyConfig.addFilter("listClone", (list) => [...list]);
 
 
-    eleventyConfig.addFilter("getRandom2", function(items,avoid) {
-      /*
-      this filter assumes items are pages
-      we need to loop until we don't pick avoid,
-      */
-      if(!items.length || items.length < 2) return;
+  eleventyConfig.addFilter("shuffle", shuffle);
 
-      let selected = items[Math.floor(Math.random() * items.length)];
-      while(selected.url === avoid.url) {
-        selected = items[Math.floor(Math.random() * items.length)];
-      }
-      return selected;
-    });
+  return {
+    dir: {
+      input: "src",
+      output: "dist"
+    }
 
-
-
-    return {
-        dir: {
-            input: "src",
-            output: "dist"
-        }
-
-    };
+  };
 
 }
